796417d324bc0e32fa5ccfa2d49b85116552146e
yilunzhou <1694962318@qq.com>
2022-07-18 23:04:54 +0800

    就反射的一些性质做一个测试类

---
+++ chapter4/src/main/java/com/smart/reflect/ReflectTest.java
@@ -0,0 +1,21 @@
+package com.smart.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class ReflectTest {
+    public static Car initByDefaultConst() throws Throwable {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Class<?> clazz = loader.loadClass("com.smart.reflect.Car");
+        Constructor<?> cons = clazz.getDeclaredConstructor();
+        Car car = (Car) cons.newInstance();
+        Method setBrand = clazz.getMethod("setBrand", String.class);
+        setBrand.invoke(car,"红旗");
+        return car;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Car car = initByDefaultConst();
+        System.out.println(car);
+    }
+}
